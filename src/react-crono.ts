0f94d42669c444a32b1dcbf90f8034e18e91e259
Prabhu <prabhu.m.murthy@gmail.com>
2020-08-29 16:46:26 +0530

    fixing tsconfig error

--- src/react-crono.ts
+++ src/react-crono.ts
@@ -1,7 +1,5 @@
 import ReactCrono from "./components/index";
 import { TimelineItemModel } from 'react-crono/dist/models/TimelineItemModel';
 
-export {
-  ReactCrono as Crono,
-  TimelineItemModel as TimelineItem
-}
+export { ReactCrono as Crono };
+export type { TimelineItemModel as TimelineItem };
