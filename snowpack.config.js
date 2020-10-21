97f223f08cbe2ef3a9765b65f58fa6ffcb21034d
Prabhu <prabhu.m.murthy@gmail.com>
2020-10-21 10:44:26 +0530

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
