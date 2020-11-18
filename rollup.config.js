46f39b6117b1fac0bf6a5cf5a555ca6816c1444d
Prabhu <prabhu.m.murthy@gmail.com>
2020-11-18 16:43:30 +0530

    rollup updates - adding terser

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import typescript from "rollup-plugin-typescript2";
 import cssnano from "cssnano";
 import postcss from "rollup-plugin-postcss"
 import pkg from "./package.json";
+import { terser } from "rollup-plugin-terser";
 
 const banner = `/*
  * ${pkg.name}
@@ -69,6 +70,7 @@ export default {
     }),
     common(),
     resolve(),
+    terser()
   ],
   external: [
     "react",
