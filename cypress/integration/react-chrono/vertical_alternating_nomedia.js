dd4af489add246e7dedbe85d6ec66e8a4dfda570
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2021-12-29 01:31:19 +0530

    fixing e2e tests

--- cypress/integration/react-chrono/vertical_alternating_nomedia.js
+++ cypress/integration/react-chrono/vertical_alternating_nomedia.js
@@ -43,7 +43,7 @@ context('Chrono.Vertical.Alternating', () => {
       .find('.card-description>p')
       .should(
         'contain',
-        'On 10 May 1940, Hitler began his long-awaited offensive in the west by invading neutral Holland and Belgium and attacking northern France. Holland capitulated after only five days of fighting, and the Belgians surrendered on 28 May. With the success of the German ‘Blitzkrieg’, the British Expeditionary Force and French troops were in danger of being cut off and destroyed.',
+        'On 10 May 1940, Hitler began his long-awaited offensive in the west',
       );
   });
 
