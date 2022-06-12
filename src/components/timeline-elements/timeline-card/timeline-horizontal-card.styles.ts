373e524185cae71bf6cb170649c3e74173bfed3f
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-06-12 12:07:02 +0530

    Replacing emotion with styled components (#257)

--- src/components/timeline-elements/timeline-card/timeline-horizontal-card.styles.ts
+++ src/components/timeline-elements/timeline-card/timeline-horizontal-card.styles.ts
@@ -1,5 +1,4 @@
-import { keyframes } from '@emotion/react';
-import styled from '@emotion/styled';
+import styled, { keyframes } from 'styled-components';
 import { Theme } from '../../../models/Theme';
 
 export const Wrapper = styled.div`
--- src/components/timeline/timeline.style.ts
+++ src/components/timeline/timeline.style.ts
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled';
+import styled from 'styled-components';
 import { Theme } from '../../models/Theme';
 import { TimelineMode } from '../../models/TimelineModel';
 
