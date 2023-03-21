5e4bc12e875f7a3a135cca8afe7506201b27fa9b
Restyled.io <commits@restyled.io>
2023-03-21 19:05:50 +0000

    Restyled by clang-format

--- cypress/e2e/react-chrono/horizontal_spec.js
+++ cypress/e2e/react-chrono/horizontal_spec.js
@@ -1,9 +1,7 @@
 /// <reference types="cypress" />
 
 context('Chrono.Horizontal.Basic', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:4444/horizontal');
-  });
+  beforeEach(() => { cy.visit('http://localhost:4444/horizontal'); });
 
   it('check length', () => {
     cy.get('.timeline-horz-item-container').should('have.length', 13);
