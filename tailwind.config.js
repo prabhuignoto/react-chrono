ce4fd3031ce01c7dfe6ca3caa10a0fef0b9eed82
Restyled.io <commits@restyled.io>
2023-03-27 17:39:44 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content : [ './src/**/*.{jsx,tsx,ts,js}' ],
-  plugins : [],
-  theme : {
-    extend : {},
+  content: ['./src/**/*.{jsx,tsx,ts,js}'],
+  plugins: [],
+  theme: {
+    extend: {},
   },
 };
