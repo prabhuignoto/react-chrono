29fff0a4f26d531a6f7b425220823dd1ea82bc93
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2023-06-14 10:02:46 +0530

    chore: updates

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     coverage: {
       clean: true,
       enabled: true,
-      provider: "c8",
+      provider: "v8",
       reporter: ['html', 'lcov', 'clover', 'cobertura'],
       reportsDirectory: './coverage'
     },
