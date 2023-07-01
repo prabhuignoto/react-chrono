680f632cd5a0d3aa330d0c3b4fb0a12bd24e3ed3
Restyled.io <commits@restyled.io>
2023-07-01 08:27:21 +0000

    Restyled by clang-format

--- webpack.config.js
+++ webpack.config.js
@@ -16,109 +16,120 @@ const banner = `/*
 const distPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
-  entry: './src/react-chrono.ts',
-  externals: {
-    react: 'react',
-    'react-dom': 'react-dom',
+  entry : './src/react-chrono.ts',
+  externals : {
+    react : 'react',
+    'react-dom' : 'react-dom',
   },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        test: /\.(ts|tsx|jsx|js)$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              plugins: [
-                '@babel/plugin-transform-runtime',
-                '@babel/plugin-proposal-optional-chaining',
-                [
-                  'babel-plugin-styled-components',
-                  { displayName: false, fileName: false, ssr: true },
-                ],
-              ],
-            },
-          },
-          {
-            loader: 'esbuild-loader',
-            options: {
-              format: 'esm',
-              loader: 'tsx',
-              tsconfigRaw: require('./tsconfig.cjs.json'),
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
+  mode : 'production',
+  module : {
+    rules :
+          [
+            {
+              exclude : /node_modules/,
+              test : /\.(ts|tsx|jsx|js)$/,
+              use :
                   [
-                    'postcss-preset-env',
                     {
-                      features: {
-                        'nesting-rules': true,
+                      loader : 'babel-loader',
+                      options : {
+                        plugins :
+                                [
+                                  '@babel/plugin-transform-runtime',
+                                  '@babel/plugin-proposal-optional-chaining',
+                                  [
+                                    'babel-plugin-styled-components',
+                                    {
+                                      displayName : false,
+                                      fileName : false,
+                                      ssr : true
+                                    },
+                                  ],
+                                ],
                       },
-                      stage: 0,
+                    },
+                    {
+                      loader : 'esbuild-loader',
+                      options : {
+                        format : 'esm',
+                        loader : 'tsx',
+                        tsconfigRaw : require('./tsconfig.cjs.json'),
+                      },
                     },
                   ],
-                  'autoprefixer',
-                  ['cssnano', { preset: 'default' }],
-                ],
-              },
             },
-          },
-        ],
-      },
-    ],
+            {
+              test : /\.css$/,
+              use :
+                  [
+                    'style-loader',
+                    'css-loader',
+                    {
+                      loader : 'postcss-loader',
+                      options : {
+                        postcssOptions : {
+                          plugins :
+                                  [
+                                    [
+                                      'postcss-preset-env',
+                                      {
+                                        features : {
+                                          'nesting-rules' : true,
+                                        },
+                                        stage : 0,
+                                      },
+                                    ],
+                                    'autoprefixer',
+                                    [ 'cssnano', {preset : 'default'} ],
+                                  ],
+                        },
+                      },
+                    },
+                  ],
+            },
+          ],
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-            drop_debugger: true,
-          },
-          // extractComments: false,
-          format: {
-            comments: false,
-          },
-        },
-      }),
-    ],
+  optimization : {
+    minimize : true,
+    minimizer :
+              [
+                new TerserPlugin({
+                  terserOptions : {
+                    compress : {
+                      drop_console : true,
+                      drop_debugger : true,
+                    },
+                    // extractComments: false,
+                    format : {
+                      comments : false,
+                    },
+                  },
+                }),
+              ],
   },
-  output: {
-    filename: `${pkg.name}.js`,
-    library: {
-      type: 'commonjs2',
+  output : {
+    filename : `${pkg.name}.js`,
+    library : {
+      type : 'commonjs2',
     },
-    libraryTarget: 'commonjs2',
-    path: distPath,
+    libraryTarget : 'commonjs2',
+    path : distPath,
   },
-  performance: {
-    hints: false,
+  performance : {
+    hints : false,
   },
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ from: 'README.md', to: distPath }],
-    }),
-    new webpack.BannerPlugin({
-      banner,
-      raw: true,
-    }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
+  plugins :
+          [
+            new CopyPlugin({
+              patterns : [ {from : 'README.md', to : distPath} ],
+            }),
+            new webpack.BannerPlugin({
+              banner,
+              raw : true,
+            }),
+          ],
+  resolve : {
+    extensions : [ '.ts', '.tsx', '.js', '.jsx' ],
+    plugins : [ new TsconfigPathsPlugin({configFile : './tsconfig.json'}) ],
   },
 };
