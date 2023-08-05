4f98d1ec1dc2e0d5976235d074f9a6c66753908b
prabhuignoto <prabhu.m.murthy@gmail.com>
2023-08-05 14:13:43 +0530

    diabling video recording

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,27 +1,29 @@
-import { defineConfig } from "cypress";
+/* eslint-disable import/no-unused-modules */
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
     devServer: {
-      bundler: "webpack",
-      framework: "react",
+      bundler: 'webpack',
+      framework: 'react',
     },
   },
 
   defaultCommandTimeout: 10000,
 
   e2e: {
-    baseUrl: "http://localhost:4444",
+    baseUrl: 'http://localhost:4444',
 
-    excludeSpecPattern: "**/examples/*.js",
+    excludeSpecPattern: '**/examples/*.js',
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
+      return require('./cypress/plugins/index.js')(on, config);
     },
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
-  projectId: "8zb5a5",
+  projectId: '8zb5a5',
 
+  video: false,
   watchForFileChanges: false,
 });
