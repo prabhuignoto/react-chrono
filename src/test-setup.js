041830890d81f8dcfbcbebc8b594ccc04f3f2941
prabhuignoto <prabhu.m.murthy@gmail.com>
2024-01-06 17:02:22 +0530

    added unit test cases for

--- src/test-setup.js
+++ src/test-setup.js
@@ -2,4 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/dom';
 import '@testing-library/jest-dom/vitest';
