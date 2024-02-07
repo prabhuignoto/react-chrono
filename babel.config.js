884ccdc2dd183f19fc0790e04cb934052a869cb0
Restyled.io <commits@restyled.io>
2024-02-07 15:17:47 +0000

    Restyled by clang-format

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  plugins: [
-    '@babel/plugin-transform-optional-chaining',
-    '@babel/plugin-transform-typescript',
-    [
-      '@emotion',
-      {
-        // sourceMap is on by default but source maps are dead code
-        // eliminated in production
-        sourceMap: false,
-      },
-    ],
-  ],
+  plugins :
+          [
+            '@babel/plugin-transform-optional-chaining',
+            '@babel/plugin-transform-typescript',
+            [
+              '@emotion',
+              {
+                // sourceMap is on by default but source maps are dead code
+                // eliminated in production
+                sourceMap : false,
+              },
+            ],
+          ],
 };
