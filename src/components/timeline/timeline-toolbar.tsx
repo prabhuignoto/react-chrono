8c17f0315a3c57b9bf0fe4f7d20a0d31a8cf44d6
prabhuignoto <prabhu.m.murthy@gmail.com>
2024-02-21 21:05:47 +0530

    disable extra controls while slideshow is running

--- src/components/timeline/timeline-toolbar.tsx
+++ src/components/timeline/timeline-toolbar.tsx
@@ -44,6 +44,7 @@ const TimelineToolbar: FunctionComponent<TimelineToolbarProps> = ({
     toolbarPosition,
     textDensity,
     isMobile,
+    
   } = useContext(GlobalContext);
 
   // Define the toolbar items
@@ -104,7 +105,7 @@ const TimelineToolbar: FunctionComponent<TimelineToolbarProps> = ({
         onToggleDarkMode={toggleDarkMode}
         onPaused={onPaused}
       />
-      <ToolbarExtraControl $isMobile={isMobile}>
+      <ToolbarExtraControl $isMobile={isMobile} $slideShowRunning={slideShowRunning}>
         <ToolbarExtraControlChild key="quick-jump">
           {enableQuickJump ? (
             <QuickJump
--- src/components/timeline/timeline.style.ts
+++ src/components/timeline/timeline.style.ts
@@ -161,12 +161,16 @@ export const ToolbarWrapper = styled.div<{ position: 'top' | 'bottom' }>`
   order: ${(p) => (p.position === 'top' ? 0 : 1)};
 `;
 
-export const ToolbarExtraControl = styled.ul<{ $isMobile: boolean }>`
+export const ToolbarExtraControl = styled.ul<{
+  $isMobile: boolean;
+  $slideShowRunning: boolean;
+}>`
   display: flex;
   align-items: center;
   list-style: none;
   margin: 0;
   padding: 0.5rem;
+  visibility: ${(p) => (p.$slideShowRunning ? 'hidden' : 'visible')};
   // margin-top: ${(p) => (p.$isMobile ? '1rem' : '0')};
 `;
 
