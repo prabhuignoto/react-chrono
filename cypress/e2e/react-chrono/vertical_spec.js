a36e8dad42cac8633fecde9286f5f6f678a1b1d2
Restyled.io <commits@restyled.io>
2024-02-27 19:33:46 +0000

    Restyled by prettier

--- cypress/e2e/react-chrono/vertical_spec.js
+++ cypress/e2e/react-chrono/vertical_spec.js
@@ -1,10 +1,13 @@
 /// <reference types="cypress" />
 
 context('Chrono.Vertical.Basic', () => {
-  beforeEach(() => { cy.visit('http://localhost:4444/vertical-basic'); });
+  beforeEach(() => {
+    cy.visit('http://localhost:4444/vertical-basic');
+  });
 
-  it('check length',
-     () => { cy.get('.vertical-item-row').should('have.length', 13); });
+  it('check length', () => {
+    cy.get('.vertical-item-row').should('have.length', 13);
+  });
 
   it('check card elements', () => {
     cy.get('.vertical-item-row').first().children().should('have.length', 3);
@@ -15,54 +18,56 @@ context('Chrono.Vertical.Basic', () => {
   });
 
   it('check timeline card contents', () => {
-    cy.get('.vertical-item-row').eq(3).find('.timeline-card-content').within(() => {
-      cy.get('.rc-card-title').should('contain', 'Pearl Harbor');
-      cy.get('.rc-card-subtitle')
-          .should(
-              'contain',
-              'The destroyer USS Shaw explodes in dry dock after being hit by Japanese aircraft',
-          );
-    });
+    cy.get('.vertical-item-row')
+      .eq(3)
+      .find('.timeline-card-content')
+      .within(() => {
+        cy.get('.rc-card-title').should('contain', 'Pearl Harbor');
+        cy.get('.rc-card-subtitle').should(
+          'contain',
+          'The destroyer USS Shaw explodes in dry dock after being hit by Japanese aircraft',
+        );
+      });
   });
 
   it('check card title', () => {
     cy.get('.vertical-item-row')
-        .eq(0)
-        .find('.rc-card-title')
-        .should('contain', 'Dunkirk');
+      .eq(0)
+      .find('.rc-card-title')
+      .should('contain', 'Dunkirk');
   });
 
   it('check card description items count(string array)', () => {
     cy.get('.vertical-item-row')
-        .eq(0)
-        .find('.card-description span')
-        .should('have.length', 2);
+      .eq(0)
+      .find('.card-description span')
+      .should('have.length', 2);
   });
 
   it('check card description', () => {
     cy.get('.vertical-item-row')
-        .eq(0)
-        .find('.card-description span')
-        .eq(0)
-        .should(
-            'contain',
-            'On 10 May 1940, Hitler began his long-awaited offensive in the west by invading neutral Holland and Belgium and attacking northern France.',
-        );
+      .eq(0)
+      .find('.card-description span')
+      .eq(0)
+      .should(
+        'contain',
+        'On 10 May 1940, Hitler began his long-awaited offensive in the west by invading neutral Holland and Belgium and attacking northern France.',
+      );
   });
 
   it('check card sub title', () => {
     cy.get('.vertical-item-row')
-        .eq(1)
-        .find('.rc-card-subtitle')
-        .should('contain', 'RAF Spitfire pilots scramble for their planes');
+      .eq(1)
+      .find('.rc-card-subtitle')
+      .should('contain', 'RAF Spitfire pilots scramble for their planes');
   });
 
   it('check card active', () => {
     cy.get('.vertical-item-row')
-        .eq(1)
-        .find('.timeline-card-content')
-        .click()
-        .should('have.class', 'active');
+      .eq(1)
+      .find('.timeline-card-content')
+      .click()
+      .should('have.class', 'active');
   });
 
   // it('check read more action', () => {
@@ -82,15 +87,15 @@ context('Chrono.Vertical.Basic', () => {
     cy.get('.timeline-main-wrapper').scrollTo('bottom');
     cy.wait(1000);
     cy.get('.vertical-item-row')
-        .last()
-        .find('.card-content-wrapper')
-        .should('have.class', 'visible');
+      .last()
+      .find('.card-content-wrapper')
+      .should('have.class', 'visible');
 
     cy.get('.timeline-main-wrapper').scrollTo('top');
     cy.wait(1000);
     cy.get('.vertical-item-row')
-        .first()
-        .find('.card-content-wrapper')
-        .should('have.class', 'visible');
+      .first()
+      .find('.card-content-wrapper')
+      .should('have.class', 'visible');
   });
 });
