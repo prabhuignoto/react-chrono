b6727acc208b6898c5810a1a028922a2606184ff
Restyled.io <commits@restyled.io>
2024-03-04 11:44:27 +0000

    Restyled by clang-format

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  plugins: [
-    '@babel/plugin-transform-optional-chaining',
-    '@babel/plugin-transform-typescript',
-    'babel-plugin-jsx-remove-data-test-id',
-    [
-      '@emotion',
-      {
-        // sourceMap is on by default but source maps are dead code
-        // eliminated in production
-        sourceMap: false,
-      },
-    ],
-  ],
+  plugins :
+          [
+            '@babel/plugin-transform-optional-chaining',
+            '@babel/plugin-transform-typescript',
+            'babel-plugin-jsx-remove-data-test-id',
+            [
+              '@emotion',
+              {
+                // sourceMap is on by default but source maps are dead code
+                // eliminated in production
+                sourceMap : false,
+              },
+            ],
+          ],
 };
