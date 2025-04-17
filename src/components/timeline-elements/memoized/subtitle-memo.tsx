503d659c25431304ba97aa2395d98769d24fd6f9
prabhuignoto <prabhu.m.murthy@gmail.com>
2025-04-17 15:56:32 +0530

    feat: add SonarLint configuration and improve type safety in subtitle component
    - Introduced a new SonarLint configuration file for better code quality analysis.
    - Updated subtitle-memo component to ensure type safety by explicitly casting content as a string.

--- src/components/timeline-elements/memoized/subtitle-memo.tsx
+++ src/components/timeline-elements/memoized/subtitle-memo.tsx
@@ -25,7 +25,7 @@ const SubTitleMemo = React.memo<Content>(
       >
         {searchTerm ? (
           <TextHighlighter
-            text={content}
+            text={content as string}
             searchTerm={searchTerm}
             theme={theme}
           />
