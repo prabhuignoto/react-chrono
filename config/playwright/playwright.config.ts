16718cd19b006cff649c8cea8f04aa0368c40687
prabhuignoto <prabhu.m.murthy@gmail.com>
2025-12-09 10:55:17 +0530

    Add Firefox support to Playwright configuration

--- config/playwright/playwright.config.ts
+++ config/playwright/playwright.config.ts
@@ -77,6 +77,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: "firefox",
+      use: { ...devices['Desktop Firefox'] },
+    }
   ],
 
   /* Run your local dev server before starting the tests */
